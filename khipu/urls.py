3ac57d3cc3ef63d928f260b5b9d6a02ceae54b96
Javier Wilson <javier.wilson@gmail.com>
2014-02-11 19:13:34 -0600

    iniciando el proyecto

---
+++ khipu/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'khipu.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('zinnia.urls')),
+    url(r'^comments/', include('django.contrib.comments.urls')),
+)
